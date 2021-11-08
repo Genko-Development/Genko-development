79a977786a8c913f105e0dcf61b9db6180dc8789
Jelle Kootwijk <kootwijkjelle@gmail.com>
2021-11-08 19:15:15 +0100

    Added youtube song player
    Added youtube song player

--- commands/join.js
+++ commands/join.js
@@ -1,16 +1,66 @@
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
+
 module.exports = {
     name: 'join',
     description: "Bot joins your call",
-    execute(message, args) {
-        if (!message.member.voice.channel) return message.channel.send("Please connect to a voice channel!"); //If you are not in the voice channel, then return a message
-        message.member.voice.channel.join();
-
-        // const resource = createAudioResource('./audio/audiofile.mp3');
-        // const dispatcher = connection.play('./audio/audiofile.mp3');
-        // player.play(resource);
-        // dispatcher.on("end", end => {
-        //     voiceChannel.leave();
-        // });
+    async execute(message, args) {
+        const voiceChannel = message.member.voice.channel;
+ 
+        if (!voiceChannel) return message.channel.send('You need to be in a channel to execute this command!');
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissins');
+        if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissins');
+        if (!args.length) return message.channel.send('You need to send the second argument!');
+ 
+        const validURL = (str) =>{
+            var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+            if(!regex.test(str)){
+                return false;
+            } else {
+                return true;
+            }
+        }
+ 
+        if(validURL(args[0])){
+ 
+            const  connection = await voiceChannel.join();
+            const stream  = ytdl(args[0], {filter: 'audioonly'});
+ 
+            connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () =>{
+                voiceChannel.leave();
+                message.channel.send('leaving channel');
+            });
+ 
+            await message.reply(`:thumbsup: Now Playing ***Your Link!***`)
+ 
+            return
+        }
+ 
+        
+        const  connection = await voiceChannel.join();
+ 
+        const videoFinder = async (query) => {
+            const videoResult = await ytSearch(query);
+ 
+            return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+ 
+        }
+ 
+        const video = await videoFinder(args.join(' '));
+ 
+        if(video){
+            const stream  = ytdl(video.url, {filter: 'audioonly'});
+            connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () =>{
+                voiceChannel.leave();
+            });
+ 
+            await message.reply(`:thumbsup: Now Playing ***${video.title}***`)
+        } else {
+            message.channel.send('No video results found');
+        }
 
 
     }

59cca7d4e6b87f15c8142503b45f0854a62ce940
Tjeerd Kootwijk <tjeerdgaming@gmail.com>
2022-03-15 15:32:20 +0100

    problem
    -cant make it work

--- commands/games/pokemon.js
+++ commands/games/pokemon.js
@@ -10,10 +10,12 @@ module.exports = {
 
         await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=1126`)
             .then((response) => {
-                check(response.data, args);
+                check(response.data, args,message);
+
             }).catch((error) => {
                 console.log(error)
             });
+            
 
         // for (item in data) {
         //     if (args[0] == data.results.name) {
@@ -36,17 +38,54 @@ module.exports = {
         // })
 
     }
-
 }
+// Math.floor((Math.random() * 10) + 1);
 
-function check(data, args) {
-    console.log(data.results.length)
+function check(data, args, message) {
     for (let i = 0; i < data.results.length; i++) {
         let pokemonName = data.results[i].name;
         str = args[0].replace(/\s+/g, '-').toLowerCase();
         if (pokemonName == str) {
             console.log("wejow")
+            message.channel.send("Ready!")
+            found = true
             break;
+        } else{
+            found = false
         }
+    }
+    if(found == false){
+        message.channel.send("That is not a real pokemon")
+    }
+    if(found == true)
+    {   
+        let filter = m => m.author.id === message.author.id
+        console.log(epic)
+        message.channel.send(`Choose an attack`).then(() => {
+            message.channel.awaitMessages(filter, {
+                max: 1,
+                time: 30000,
+                errors: ['time']
+              })
+              .then((message) => {
+                // messageFirst = FilterMessage.first()
+                playerNumber = Math.floor((Math.random() * 10) + 1);
+                EnemyNumber = Math.floor((Math.random() * 10) + 1);
+                if(playerNumber > EnemyNumber){
+                    message.channel.send(`You won`)
+                } 
+                if(playerNumber < EnemyNumber){
+                    message.channel.send(`You Lost`)
+                }
+                if(playerNumber == EnemyNumber){
+                    message.channel.send(`Draw`)
+                }
+
+              })
+              .catch(collected => {
+                  console.log(error)
+                message.channel.send('Timeout');
+            });
+        })        
     }
 }
